27c8bfc59c99e6f63e60043568c0586614672d2f
Chris Leclair <xcvgvcx@gmail.com>
2023-06-03 21:19:41 -0700

    just a bit of scaffolding

---
+++ Assets/CommonCoreModules/TurnBasedBattleSystem/Scripts/TBBSSceneController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonCore.TurnBasedBattleSystem
+{
+    public class TBBSSceneController : BaseSceneController
+    {
+        
+
+        
+    }
+}
+
+
